a4a05b7ffdc3816d0d6573eb88dabb35203c9a6a
NityanDesai <nityandesai@gmail.com>
2021-03-11 13:52:57 +0530

    Fixed open external links & error pages

--- app/src/main/java/com/example/itmvubustracking/MainActivity.java
+++ app/src/main/java/com/example/itmvubustracking/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.itmvubustracking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Message;
 import android.view.Window;
 import android.view.WindowManager;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -18,11 +21,23 @@ public class MainActivity extends AppCompatActivity {
 //        requestWindowFeature(Window.FEATURE_NO_TITLE);
 //        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.getSupportActionBar().hide();
-        myWebView = (WebView)findViewById(R.id.web);
+        myWebView = (WebView) findViewById(R.id.web);
+        myWebView.setBackgroundColor(Color.BLACK);
+        myWebView.setWebViewClient(new WebViewClient());
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setSupportMultipleWindows(true);
         myWebView.loadUrl("https://itmvubustracking.pythonanywhere.com");
-        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.setWebChromeClient(new WebChromeClient() {
+            @Override public boolean onCreateWindow(WebView view, boolean dialog, boolean userGesture, Message resultMsg)
+            {
+                WebView newWebView = new WebView(view.getContext());
+                WebView.WebViewTransport transport = (WebView.WebViewTransport) resultMsg.obj;
+                transport.setWebView(newWebView);
+                resultMsg.sendToTarget();
+                return true;
+            }
+        });
     }
 
     @Override
